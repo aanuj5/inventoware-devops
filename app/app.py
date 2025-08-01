0c59ecac2dd33eaa730ebe85ebf27adc897af0be
aanuj5 <aanujantil5@gmail.com>
2025-08-01 11:41:01 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "✅ InventoWare Legacy App Running in Docker!"
+    return 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
